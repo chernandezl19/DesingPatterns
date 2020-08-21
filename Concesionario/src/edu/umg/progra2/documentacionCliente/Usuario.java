cb6ec2d06d14dcec58cf8cccad5484f86975366d
HP <HP@DESKTOP-P1S608N>
2020-08-21 15:42:47 -0600

    .

--- Concesionario/src/edu/umg/progra2/documentacionCliente/Usuario.java
+++ Concesionario/src/edu/umg/progra2/documentacionCliente/Usuario.java
@@ -1,18 +1,17 @@
 package edu.umg.progra2.documentacionCliente;
 
 public class Usuario {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+// TODO Auto-generated method stub
 		DocumentacionEnBlanco documentacionEnBlanco = DocumentacionEnBlanco.Instance();
 		documentacionEnBlanco.incluye(new OrdenDePedido());
 		documentacionEnBlanco.incluye(new CertificacionCesion());
 		documentacionEnBlanco.incluye(new SolicitudMatriculacion());
-		//creacion documentacion para 2 nuevos clientes
+		documentacionEnBlanco.incluye(new SolicitudTarjetaCirculacion());
+//creacion documentacion para 2 nuevos clientes
 		DocumentacionCliente documentacionCliente1 = new DocumentacionCliente("Juan Perez");
 		DocumentacionCliente documentacionCliente2 = new DocumentacionCliente("Pedro Molina");
 		documentacionCliente1.visualiza();
 		documentacionCliente2.visualiza();
 	}
-
 }
