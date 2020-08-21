6de1afafd4e3912aefb61a7aadd01442fed12209
jaqueline <jaqueline@192.168.1.33>
2020-08-21 17:10:54 -0600

    se agregaron las clases que se pedian agregar en el proyecto

---
+++ Concesionario/src/edu/umg/progra2/catalogos/FabricaVehiculoHibrido.java
@@ -0,0 +1,18 @@
+package edu.umg.progra2.catalogos;
+
+public class FabricaVehiculoHibrido implements FabricaVehiculo {
+
+	@Override
+	public Automovil creaAutomovil(String modelo, String color, int potencia, double espacio) {
+		
+		return new AutomovilHibrido(modelo, color, potencia, espacio);
+	}
+
+	@Override
+	public Scooter creaScooter(String modelo, String color, int potencia) {
+		
+		return new ScooterHibrido(modelo, color, potencia);
+	}
+
+}
+
---
+++ Concesionario/src/edu/umg/progra2/pedido/PedidoCheque.java
@@ -0,0 +1,21 @@
+package edu.umg.progra2.pedido;
+
+public class PedidoCheque extends Pedido {
+	
+	public PedidoCheque(double importe) {
+		super(importe);
+	}
+
+	@Override
+	public boolean valida() {
+		return true;
+	}
+
+	@Override
+	public void paga() {
+		System.out.println("el pago del pedido por cheque de: "+importe+"se ha realizado con exito");
+
+	}
+
+
+}
