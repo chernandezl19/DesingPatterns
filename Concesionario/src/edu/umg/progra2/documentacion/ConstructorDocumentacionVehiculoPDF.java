e2cdcde07c8e022b56b486dd8d94a4d83cb0729a
Ender_Carias <endercarias@gmail.com>
2020-08-21 17:33:15 -0600

    tare5

---
+++ Concesionario/src/edu/umg/progra2/documentacion/ConstructorDocumentacionVehiculoPDF.java
@@ -0,0 +1,29 @@
+package edu.umg.progra2.documentacion;
+
+public class ConstructorDocumentacionVehiculoPDF extends ConstructorDocumentacionVehiculo {
+
+	public ConstructorDocumentacionVehiculoPDF() {
+		documentacion = new DocumentacionPDF();
+	}
+
+	@Override
+	public void construyeSolicitudPedido(String nombreCliente) {
+		String documento = "<PDF>Solicitud  de pedido de cliente +" + nombreCliente + "</PDF>";
+		documentacion.agregarDocumento(documento);
+
+	}
+
+	@Override
+	public void construyeSolicitudMatriculacion(String nombreSolicitante) {
+		String documento = "<PDF>Solicitud  de matriculacion de solicitante +" + nombreSolicitante + "</PDF>";
+		documentacion.agregarDocumento(documento);
+
+	}
+
+	@Override
+	public void construyeSolicitudTarjetaCirculacion(String nombreSolicitante) {
+		String documento = "<PDF>Solicitud  para Tarjeta de Circulacion de solicitante +" + nombreSolicitante + "</PDF>";
+		documentacion.agregarDocumento(documento);
+
+	}
+}
---
+++ Concesionario/src/edu/umg/progra2/pedido/ClienteContado.java
@@ -0,0 +1,10 @@
+package edu.umg.progra2.pedido;
+
+public class ClienteContado extends Cliente {
+
+	@Override
+	protected Pedido creaPedido(double importe) {
+		// TODO Auto-generated method stub
+		return new PedidoContado(importe);
+	}
+}
