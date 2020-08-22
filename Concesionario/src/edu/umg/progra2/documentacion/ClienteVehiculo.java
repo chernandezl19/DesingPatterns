fcace3cada14cd7ef3c3b0b85cac0f5e7c6e84f3
pablo <remagforce@gmail.com>
2020-08-22 09:31:29 -0600

    comentario

--- Concesionario/src/edu/umg/progra2/documentacion/ClienteVehiculo.java
+++ Concesionario/src/edu/umg/progra2/documentacion/ClienteVehiculo.java
@@ -20,7 +20,7 @@ public class ClienteVehiculo {
 			constructor = new ConstructorDocumentacionVehiculoTxt();
 		}
 
-		//comentario
+		//comentar
 		Vendedor vendedor = new Vendedor(constructor);
 		Documentacion documentacion = vendedor.construye("Juan Perez");
 		
