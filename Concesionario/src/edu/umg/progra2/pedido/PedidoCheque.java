61c5a44aed81e4cb1a31fc6f165379f61d55449d
rafae <rafae@LAPTOP-2NGGO5UG>
2020-08-21 22:14:42 -0600

    Cambios realizados en la tarea

---
+++ Concesionario/src/edu/umg/progra2/pedido/PedidoCheque.java
@@ -0,0 +1,24 @@
+package edu.umg.progra2.pedido;
+
+public class PedidoCheque extends Pedido{
+	
+	
+	public PedidoCheque(double importe) {
+		super(importe);
+	}
+
+	@Override
+	public boolean valida() {
+		return true;
+	}
+
+	@Override
+	public void paga() {
+		System.out.println("el pago del pedido por importe de: "+importe+"se ha realizado con exito");
+
+	}
+
+	
+	
+	
+}
