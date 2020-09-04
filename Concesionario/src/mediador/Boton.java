ba881c6607e993f0258ab4d7f18e25f84c98512e
CarlosVasquezDB <charlesvas16@outlook.com>
2020-09-04 11:43:09 -0600

    Clases Control, ZonaInf. y Boton agregadas

---
+++ Concesionario/src/mediador/Boton.java
@@ -0,0 +1,11 @@
+package mediador;
+
+import java.util.Scanner;
+
+public class Boton extends Control{
+	Scanner reader = new Scanner(System.in);
+	
+	public Boton(String nombre) {
+		super(nombre);
+	}
+}
---
+++ Concesionario/src/mediador/Control.java
@@ -0,0 +1,41 @@
+package mediador;
+
+public abstract class Control {
+	
+	protected String valor = "";
+	public Formulario director;
+	public String nombre;
+	
+	public Control(String nombre) {
+		setNombre(nombre);
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	protected void setNombre (String nombre) {
+		this.nombre = nombre;
+	}
+	
+	protected Formulario getDirector() {
+		return director;
+	}
+	
+	public void setDirector(Formulario director) {
+		this.director = director;
+	}
+	
+	public String getValor() {
+		return valor;
+	}
+	
+	protected void setValor(String valor) {
+		this.valor = valor;
+	}
+	
+	public abstract void informa();
+	
+	protected void modifica() {
+		getDirector().controlModificado(this);
+	}
