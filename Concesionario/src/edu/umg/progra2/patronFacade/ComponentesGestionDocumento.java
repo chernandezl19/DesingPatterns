b54a0990de099dda18e262969ccda25ee59898e6
Yesseli <yesselimateo@gmail.com>
2020-09-04 22:16:18 -0600

    Tarea 6

---
+++ Concesionario/src/edu/umg/progra2/patronFacade/ComponentesGestionDocumento.java
@@ -0,0 +1,9 @@
+package edu.umg.progra2.patronFacade;
+
+public class ComponentesGestionDocumento implements GestionDocumento {
+
+	public String documento(int indice) {
+		return "Documento número " + indice;
+	}
+
+}
